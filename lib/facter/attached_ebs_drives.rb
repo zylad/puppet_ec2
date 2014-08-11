6526287e6ac1b50beb6571778fba62fd19095295
Ari Adair <patrick.adair@adroll.com>
2014-08-11 13:44:49 -0700

    Completely change the EBS drive facts to structured format.

--- lib/facter/attached_ebs_drives.rb
+++ lib/facter/attached_ebs_drives.rb
@@ -4,6 +4,7 @@ require 'aws-sdk'
 region            = Facter.value('ec2_region')
 instance_id       = Facter.value('ec2_instance_id')
 
+ebs_drive_data    = {}
 total_volumes     = []
 non_root_volumes  = []
 
@@ -12,48 +13,34 @@ AWS.config(region: region)
 resp = AWS.ec2.client.describe_volumes(filters: [{name: 'attachment.instance-id', values: [instance_id]}])
 
 resp.data()[:volume_set].each { |vol|
-  clean_drive_name  = vol[:volume_id].gsub(/-/, "_")
   attachment        = vol[:attachment_set][0]
+  drive             = {}
 
-  total_volumes << clean_drive_name
+  total_volumes << vol[:volume_id]
 
-  Facter.add("ec2_ebs_#{clean_drive_name}_size_gb") {
-    setcode { vol[:size] }
-  }
+  drive['size_gb'] = vol[:size]
 
   ## If we have a snapshot ID to go along with this drive, include
   ## it for consumption by other systems.
-  Facter.add("ec2_ebs_#{clean_drive_name}_snapshot_id") {
-    setcode { vol[:snapshot_id] }
-  }
+  drive['snapshot_id'] = vol[:snapshot_id]
 
   ## Determine what kind of EBS drive this is.
-  Facter.add("ec2_ebs_#{clean_drive_name}_type") {
-    setcode { vol[:volume_type] }
-  }
+  drive['type'] = vol[:volume_type]
 
-  if vol["VolumeType"] == "io1" then
-    Facter.add("ec2_ebs_#{clean_drive_name}_iops") {
-      setcode { vol[:iops] }
-    }
+  if vol['VolumeType'] == 'io1' then
+    drive['iops'] = vol[:iops]
   end
 
-  Facter.add("ec2_ebs_#{clean_drive_name}_mount_point") {
-    setcode { attachment[:device].gsub(/s/, 'xv') }
-  }
+  drive['mount_point'] = attachment[:device].gsub(/s/, 'xv')
 
   ## We want to mark the root EBS drive as special.
-  if Facter.value("ec2_ebs_#{clean_drive_name}_mount_point") == '/dev/xvda1' then
-    Facter.add("ec2_ebs_#{clean_drive_name}_is_root_device") {
-      setcode { true }
-    }
+  if drive['mount_point'] == '/dev/xvda1' then
+    drive['is_root_device'] = true
   else
     non_root_volumes << clean_drive_name
   end
 
-  Facter.add("ec2_ebs_#{clean_drive_name}_delete_on_termination") {
-    setcode { attachment[:delete_on_termination] }
-  }
+  drive['delete_on_termination'] = attachment[:delete_on_termination]
 
   ## Iterate through the tags and create a better data representation.
   volume_tags = {}
@@ -61,9 +48,12 @@ resp.data()[:volume_set].each { |vol|
     volume_tags[tag[:key]] = tag[:value]
   }
 
-  Facter.add("ec2_ebs_#{clean_drive_name}_tags") {
-    setcode { volume_tags }
-  }
+  drive['tags'] = volume_tags
+  ebs_drive_data[vol[:volume_id]] = drive
+}
+
+Facter.add("ec2_ebs_drive_data") {
+  setcode { ebs_drive_data }
 }
 
 Facter.add("ec2_ebs_volumes") {
