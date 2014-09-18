f18ee8d8ccbc90797be973ed1af1369fc99c1f22
Ari <ari@adroll.com>
2014-09-17 17:49:32 -0700

    Much better way of resolving VPCs.

--- lib/facter/instance_metadata.rb
+++ lib/facter/instance_metadata.rb
@@ -28,12 +28,13 @@ Facter.add('ec2_public_ip') {
 
 
 ## This section will pull the VPC ID this instance is in.
+Facter.add('ec2_mac') {
+  setcode { Net::HTTP.get('169.254.169.254', 'latest/meta-data/mac') }
+}
+
 Facter.add('ec2_vpc_id') {
   setcode {
-    region      = Facter.value('ec2_region')
-    instance_id = Facter.value('ec2_instance_id')
-
-    AWS.config(region: region)
-    AWS.ec2.instances[instance_id].vpc_id
+    mac = Facter.value('ec2_mac')
+    Net::HTTP.get('169.254.169.254', "network/interfaces/macs/#{mac}/vpc-id")
   }
 }
