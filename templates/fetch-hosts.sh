a5dec74c528ead043365199d9994ef98dea5448f
Ari <ari@adroll.com>
2014-09-22 19:28:00 -0700

    Add in the proper Anchor server IP.

--- templates/fetch-hosts.sh
+++ templates/fetch-hosts.sh
@@ -2,4 +2,4 @@
 # Fetch hosts file for this instance 
 aws s3 cp s3://adroll-releases/etc_hosts/etc_hosts.<%=ec2_hosts_file %>.sh /tmp/hosts.sh
 . /tmp/hosts.sh
-echo "<%=serverip %> puppet" >> /etc/hosts
+echo "`anchor-utility anchor-server` puppet" >> /etc/hosts
